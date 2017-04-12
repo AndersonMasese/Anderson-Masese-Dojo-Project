18664f282778eb91e2ab9aa3af2e257497524733
andersonmasese <andersonmasese@gmail.com>
2017-04-12 07:51:25 -0700

    file contains the implementations for task0,1 and some functionalities in task 2

--- app/dojo.py
+++ app/dojo.py
@@ -19,22 +19,41 @@ class Dojo(object):
     def create_room(room_type,room_name):
         '''
         Check that the room does not exist and determine what type of room it is
+
         '''
-        if room_name in Dojo.all_rooms:
-            print('Room already exists')
-            return 'Room already exists'
-        elif room_type == 'O':
-            current_room = Office(room_name)
-            Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
-            Dojo.office_rooms[current_room.room_name.upper()]
-            print('Office called %s created succesfully' % room_name)
 
-        elif room_type == 'L':
-            current_room = LivingSpace(room_name)
-            Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
-            Dojo.ls_rooms[current_room.room_name.upper()]
-            print('Living space called %s created succesfully' % room_name)
+        
+        for room_name in room_name:
+            #room_name=name_of_room[0]
+            if room_name in Dojo.all_rooms:
+                print('Room already exists')
+                return 'Room already exists'
+            elif room_type == 'office':
 
+                current_room = Office(room_name)
+                Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
+                Dojo.office_rooms[current_room.room_name.upper()]
+            
+
+                print('Office called %s created succesfully' % room_name)
+
+            elif room_type == 'livingspace':
+                current_room = LivingSpace(room_name)
+                Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
+                Dojo.ls_rooms[current_room.room_name.upper()]
+                print('Living space called %s created succesfully' % room_name)
+
+            
+
+
+ 
+        
+                
+        
+        
+            
+
+            
     @staticmethod
     def add_person(firstname, lastname, position, wants_accomodation='N'):
         '''
